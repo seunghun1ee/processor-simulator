35b0c5b8fef478296898d574ad6706872c079f0f
seunghun <viper1259@gmail.com>
2021-02-27 11:13:36 +0000

    stall works fine for decode-execute

--- src/Processor3.java
+++ src/Processor3.java
@@ -14,6 +14,7 @@ public class Processor3 {
     boolean decodeBlocked = false;
     boolean executeBlocked = false;
     int executeCycle = 0;
+    int execEndCycle = 0;
 
 
     public Processor3() {
@@ -34,11 +35,37 @@ public class Processor3 {
     }
 
     private void Decode() {
-        if(executeBlocked && executeCycle > 1) { // if execute is processing previous instruction more than 1 cycle
+        if(executeCycle > 1) { // if execute is processing previous instruction more than 1 cycle
             decodeBlocked = true; // decode input is blocked
         }
-        else {
+        else { // getting overwritten too fast for MUL
             decodeBlocked = false;
+
+        }
+
+        if(!decodeBlocked) {
+            if(fetched == null) {
+                decoded = new Instruction();
+                decoded.opcode = Opcode.NOOP;
+            }
+            else {
+                decoded = fetched;
+            }
+        }
+
+        cycle++;
+    }
+
+    private void Decode2() {
+        if(cycle < execEndCycle) { // if execute is processing previous instruction more than 1 cycle
+            decodeBlocked = true; // decode input is blocked
+        }
+        else { // getting overwritten too fast for MUL
+            decodeBlocked = false;
+
+        }
+
+        if(!decodeBlocked) {
             if(fetched == null) {
                 decoded = new Instruction();
                 decoded.opcode = Opcode.NOOP;
@@ -189,11 +216,145 @@ public class Processor3 {
         executedInsts++;
     }
 
+    private void Execute2() {
+        if(!executeBlocked) { // if execute input is not blocked, update executing instruction
+            executing = decoded;
+            executeCycle = 0;
+        }
+        if(executeCycle < executing.numCycles - 1) {
+            executeBlocked = true;
+        }
+        else {
+            finishExecution(executing);
+            executeBlocked = false;
+        }
+        executeCycle++;
+        cycle++;
+    }
+
+    private void Execute3() {
+        if(!executeBlocked) {
+            executing = decoded;
+            execEndCycle = cycle + executing.numCycles;
+        }
+        if(cycle < execEndCycle - 1) {
+            executeBlocked = true;
+        }
+        else {
+            finishExecution(executing);
+            executeBlocked = false;
+        }
+        cycle++;
+    }
+
+    private void finishExecution(Instruction ins) {
+        if(ins.Rd != 0) { // register 0 is read-only
+            switch (ins.opcode) {
+                case ADD:
+                    rf[ins.Rd] = rf[ins.Rs1] + rf[ins.Rs2];
+                    break;
+                case ADDI:
+                    rf[ins.Rd] = rf[ins.Rs1] + ins.Const;
+                    break;
+                case SUB:
+                    rf[ins.Rd] = rf[ins.Rs1] - rf[ins.Rs2];
+                    break;
+                case MUL:
+                    rf[ins.Rd] = rf[ins.Rs1] * rf[ins.Rs2];
+                    break;
+                case MULI:
+                    rf[ins.Rd] = rf[ins.Rs1] * ins.Const;
+                    break;
+                case DIV:
+                    rf[ins.Rd] = rf[ins.Rs1] / rf[ins.Rs2];
+                    break;
+                case DIVI:
+                    rf[ins.Rd] = rf[ins.Rs1] / ins.Const;
+                    break;
+                case NOT:
+                    rf[ins.Rd] = ~rf[ins.Rs1];
+                    break;
+                case AND:
+                    rf[ins.Rd] = rf[ins.Rs1] & rf[ins.Rs2];
+                    break;
+                case OR:
+                    rf[ins.Rd] = rf[ins.Rs1] | rf[ins.Rs2];
+                    break;
+                case LD:
+                    rf[ins.Rd] = mem[rf[ins.Rs1] + rf[ins.Rs2]];
+                    break;
+                case LDC:
+                    rf[ins.Rd] = ins.Const;
+                    break;
+                case LDI:
+                    rf[ins.Rd] = mem[ins.Const];
+                    break;
+                case LDO:
+                    rf[ins.Rd] = mem[rf[ins.Rs1] + ins.Const];
+                    break;
+                case MV:
+                    rf[ins.Rd] = rf[ins.Rs1];
+                    break;
+                case CMP:
+                    rf[ins.Rd] = Integer.compare(rf[ins.Rs1], rf[ins.Rs2]);
+                    break;
+                case NOOP:
+                default:
+                    break;
+            }
+        }
+
+        switch (ins.opcode) { // instructions that are safe with gpr[0]
+            case ST:
+                mem[rf[ins.Rs1] + rf[ins.Rs2]] = rf[ins.Rd];
+                break;
+            case STI:
+                mem[ins.Const] = rf[ins.Rd];
+                break;
+            case STO:
+                mem[rf[ins.Rs1] + ins.Const] = rf[ins.Rd];
+                break;
+            case BR:
+                pc = ins.Const;
+                fetched = new Instruction(Opcode.NOOP,0,0,0,0);
+                break;
+            case JMP:
+                pc = pc + ins.Const - 1;
+                fetched = new Instruction(Opcode.NOOP,0,0,0,0);
+                break;
+            case JR:
+                pc = ins.Rs1;
+                fetched = new Instruction(Opcode.NOOP,0,0,0,0);
+                break;
+            case BEQ:
+                if(rf[ins.Rs1] == rf[ins.Rs2]) {
+                    pc = ins.Const;
+                    fetched = new Instruction(Opcode.NOOP,0,0,0,0);
+                }
+                break;
+            case BLT:
+                if(rf[ins.Rs1] < rf[ins.Rs2]) {
+                    pc = ins.Const;
+                    fetched = new Instruction(Opcode.NOOP,0,0,0,0);
+                }
+                break;
+            case HALT:
+                finished = true;
+                break;
+            case NOOP:
+            default:
+                break;
+        }
+
+
+
+    }
+
     public void RunProcessor() {
 
         while(!finished && pc < instructions.length) {
             System.out.println("PC " + pc + " " + cycle + " number of cycles passed");
-            Execute();
+            Execute2();
             Decode();
             Fetch();
         }
