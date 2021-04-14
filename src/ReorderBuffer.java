b058b5fa753ae8917b4f283dc0577c560e815f3b
seunghun <viper1259@gmail.com>
2021-04-14 16:51:04 +0100

    remove unused fields

--- src/ReorderBuffer.java
+++ src/ReorderBuffer.java
@@ -1,9 +1,7 @@
 public class ReorderBuffer {
 
-    public boolean busy = false;
     public boolean ready = false;
     public Instruction ins = new Instruction();
-    public int state;
     public int destination;
     public int value;
     public int address;
